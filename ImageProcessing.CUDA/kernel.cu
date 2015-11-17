d401625b4b3f8550cbee120f1430d7b3313c789c
UnshiftedShark <ekgaly01@louisville.com>
2015-11-17 15:35:10 -0500

    CUDA Project Added to Solution
    Added the CUDA Project to the solution. This project should not be a
    direct dependency for MFC project. When we have finished the CUDA part
    for image processing we will then include the files needed in the MFC
    project to filter images.

---
+++ ImageProcessing.CUDA/kernel.cu
@@ -0,0 +1,19 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <stdio.h>
+
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void addKernel(int *c, const int *a, const int *b)
+{
+    
+}
+
+int main()
+{
+   
+	return 0;
+}
